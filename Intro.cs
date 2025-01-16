bf9fedb07058fdc623e0561a66552d0d9f6c84dc
Aalok Shailendra Choudhari <aalokshailendra_choudhari@epam.com>
2025-01-16 11:49:10 +0530

    1st commit

--- Intro.cs
+++ Intro.cs
@@ -2,6 +2,8 @@ class Program
     {  
         static void Main(string[] args)  
             {  
-                System.Console.WriteLine("Hello Aalok");  
+                System.Console.WriteLine("Hello Aalok"); 
+                
+                System.Console.WriteLine("1st new commit"); 
             }  
     }  
